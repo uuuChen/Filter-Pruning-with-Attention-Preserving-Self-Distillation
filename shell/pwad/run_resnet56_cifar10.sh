13d9ff3b95802ec135874c9a80319233231ed2ce
uuuChen <fredusr1@gmail.com>
2021-06-05 21:52:28 +0800

    edit shell/pwad

--- shell/pwad/run_resnet56_cifar10.sh
+++ shell/pwad/run_resnet56_cifar10.sh
@@ -7,9 +7,8 @@ python3 pwad.py \
     --schedule 60 120 160 \
     --lr-drops 0.2 0.2 0.2 \
     --prune-rates 0.6 \
-    --prune-mode filter-n-gm-1 \
-    --samp-batches 10 \
-    --dist-mode all-dist \
+    --prune-mode filter-n-g-gm-1 \
+    --samp-batches 15 \
     --s-load-model-path saves/resnet56_cifar10/initial_train/model_epochs_163.pt \
     --seed 8152 \
 
