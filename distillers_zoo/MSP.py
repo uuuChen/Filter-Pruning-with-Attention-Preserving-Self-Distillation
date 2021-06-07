83922cac26590f856435943699bb855ef338f784
uuuChen <fredusr1@gmail.com>
2021-06-07 15:02:58 +0800

    add MSP_v2

--- distillers_zoo/MSP.py
+++ distillers_zoo/MSP.py
@@ -16,13 +16,13 @@ class MultiSimilarity(nn.Module):
         s_nl = len(s_g)
         t_nl = len(t_g)
 
-        s_mtx = torch.stack([self.get_sim_matrix(s_f) for s_f in s_g])  # (s_nl, bs, bs)
-        t_mtx = torch.stack([self.get_sim_matrix(t_f) for t_f in t_g])  # (t_nl, bs, bs)
+        s_mtx = torch.stack([self.get_sim_matrix_v2(s_f) for s_f in s_g])  # (s_nl, bs, bs)
+        t_mtx = torch.stack([self.get_sim_matrix_v2(t_f) for t_f in t_g])  # (t_nl, bs, bs)
 
         s_mtx = torch.unsqueeze(s_mtx, dim=1).repeat(1, t_nl, 1, 1)  # (s_nl, t_nl, bs, bs)
         t_mtx = torch.unsqueeze(t_mtx, dim=0).repeat(s_nl, 1, 1, 1)  # (s_nl, t_nl, bs, bs)
 
-        loss = (s_mtx - t_mtx).pow(2).view(s_nl, -1).mean(1).sum()  # (1,)
+        loss = (s_mtx - t_mtx).pow(2).view(s_nl, -1).mean(1).mean()  # (1,)
         return loss
 
     def get_sim_matrix(self, f):
@@ -34,3 +34,18 @@ class MultiSimilarity(nn.Module):
         mtx = torch.matmul(n_f, torch.t(n_f))  # (bs, bs)
         n_mtx = F.normalize(mtx, dim=1)  # (bs, bs)
         return n_mtx
+
+    def get_sim_matrix_v2(self, f):
+        # --------------------------------------------
+        # Shape of f : (bs, ch, w, h)
+        # --------------------------------------------
+        a_f = self.at(f)  # (bs, w * h)
+        mtx = torch.matmul(a_f, torch.t(a_f))  # (bs, bs)
+        n_mtx = F.normalize(mtx, dim=1)  # (bs, bs)
+        return n_mtx
+
+    def at(self, f):
+        # --------------------------------------------
+        # Shape of f : (bs, ch, w, h)
+        # --------------------------------------------
+        return F.normalize(f.pow(2).mean(1).view(f.size(0), -1))  # (bs, w * h)
